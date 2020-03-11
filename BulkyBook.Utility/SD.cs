3065c23b7d10b4989b1dddbe97606dbc39123042
Zalgawi <zayd.algawi@hotmail.co.uk>
2020-03-11 11:12:50 +0000

    Enabled stored-procedure using Dapper as an example if no EntityFramework is used. Example is made for CoverTypes.

--- BulkyBook.Utility/SD.cs
+++ BulkyBook.Utility/SD.cs
@@ -6,6 +6,10 @@ namespace BulkyBook.Utility
 {
     public static class SD
     {
-
+        public const string Proc_CoverType_Create = "usp_CreateCovertype";
+        public const string Proc_CoverType_Get = "usp_GetCovertype";
+        public const string Proc_CoverType_GetAll = "usp_GetCovertypes";
+        public const string Proc_CoverType_Update = "usp_UpdateCovertype";
+        public const string Proc_CoverType_Delete = "usp_DeleteCovertype";
     }
 }
