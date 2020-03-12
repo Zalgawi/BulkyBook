23da1fa517d616ecb49a61963fc2eaa2a2fcde18
Zalgawi <zayd.algawi@hotmail.co.uk>
2020-03-12 16:30:39 +0000

    Enabled user's account locked / unlocked feature

--- BulkyBook/Areas/Admin/Controllers/UserController.cs
+++ BulkyBook/Areas/Admin/Controllers/UserController.cs
@@ -50,6 +50,26 @@ namespace BulkyBook.Areas.Admin.Controllers
 
             return Json(new { data = userList });
         }
+        [HttpPost]
+        public IActionResult LockUnlock([FromBody] string id)
+        {
+            var objFromDb = _db.ApplicationUsers.FirstOrDefault(u => u.Id == id);
+            if(objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while Locking / Unlocking" });
+            }
+            if(objFromDb.LockoutEnd != null && objFromDb.LockoutEnd > DateTime.Now)
+            {
+                //this means user is currently locked, we will unlock the account.
+                objFromDb.LockoutEnd = DateTime.Now;
+            }
+            else
+            {
+                objFromDb.LockoutEnd = DateTime.Now.AddYears(1000);
+            }
+            _db.SaveChanges();
+            return Json(new { success = true, message = "Operation Successful." });
+       }
         #endregion
     }
 }
